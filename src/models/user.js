53054faeebb2f3d1292d56650565c277eaa43a60
unknown <ajayrao895@gmail.com>
2021-03-26 07:49:39 +0530

    Added password encryption in user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -18,6 +19,22 @@ const userSchema = new mongoose.Schema({
         minlength: 7,
         trim: true
     }
+});
+
+userSchema.methods.toJSON = function () {
+    const user = this;
+    const userObject = user.toObject();
+    delete userObject.password;
+    delete userObject.__v;
+    return userObject;
+};
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
