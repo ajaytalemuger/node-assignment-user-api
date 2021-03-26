f290d0622a3545f347388aa81442b26c0f2b4436
unknown <ajayrao895@gmail.com>
2021-03-26 14:35:11 +0530

    Added request logging

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+require('./src/db/mongooseConnection');
+
 const PORT = process.env.PORT || 8000;
 const app = express();
 
-require('./src/db/mongooseConnection');
-
 const authRouter = require("./src/routers/auth");
 const userRouter = require("./src/routers/user");
+
+// To store request logging in access.log file
+let accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use(express.json());
 app.use("/auth", authRouter);
